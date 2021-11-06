02dfe74b8e34ac4f96ead3c4bfe306ab6b9aed2b
Braxton Allen <90428117+Vivonzty15@users.noreply.github.com>
2021-11-06 12:41:58 -0500

    final attempt

--- script.js
+++ script.js
@@ -7,45 +7,61 @@ let sugar = 0
 let allgb = document.getElementById("qty-gb")
 let allcc= document.getElementById('qty-cc')
 let allsugar= document.getElementById('qty-sugar')
-let total = gb + cc + sugar
+var total = document.getElementById("qty-total")
 
 document.getElementById('credit').textContent = `Created by Braxton Allen`
 
 document.getElementById('add-gb').addEventListener('click', function() {   
+    if (gb >= 0){
     gb++
-    allgb.textContent = gb
+    
+    allgb.textContent = gb ;
+    total.textContent = gb + cc + sugar;
+
+    }
 })
 
 document.getElementById('minus-gb').addEventListener('click', function() {
-    if(gb => 0){
-        gb--
+    if(gb > 0){
+    gb--
+
+    allgb.textContent = gb;
+    total.textContent = gb + cc + sugar;
+
     } 
-    allgb.textcontent = gb
 })
 
 document.getElementById('add-cc').addEventListener('click', function() {
     cc++
     allcc.textContent = cc
+    total.textContent = gb + cc + sugar
 })
 
 document.getElementById('minus-cc').addEventListener('click', function() {
-    if(cc => 0){
+    if(cc > 0){
         cc--
-    }
-    allcc.textContent = cc
     
+    allcc.textContent = cc
+    total.textContent = gb + cc + sugar
+    }
 })
 
 document.getElementById('add-sugar').addEventListener('click', function() {
     sugar++
-    allsugar.textContent = sugar
+  
+    allsugar.textContent = sugar;
+    total.textContent = gb + cc + sugar
+
+
 })
 
 document.getElementById('minus-sugar').addEventListener('click', function() {
-    if(sugar => 0){
+    if(sugar > 0){
         sugar--
-    }
+    
     allsugar.textContent = sugar
+    total.textContent = gb + cc + sugar
+    }
 })
 
 document.getElementById("qty-total").textContent = total
